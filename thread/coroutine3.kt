a81fc59067ac1a4459532a31841ee34720bc0200
sameer patil <sbpatil29@gmail.com>
2026-02-17 21:32:42 +0530

    simple call to a suspend fun using runBlocking main

---
+++ thread/coroutine3.kt
@@ -0,0 +1,24 @@
+/*please read and understand the coroutine2.kt file before looking at this code,
+ as this code is a continuation of that file.
+   
+ */
+import kotlinx.coroutines.*
+
+
+suspend fun mydata() {
+    println("I m inside coroutine")
+    println("I will wait for 2 seconds")
+    // delay is a non-blocking pause
+   // delay(2000)
+    println("routine is ending here")
+}
+
+fun main() = runBlocking {
+    // launch starts a new coroutine without blocking the main thread
+    launch {
+        mydata()
+        println("my data is paused ")
+    }
+
+    println("This is main")
+}
